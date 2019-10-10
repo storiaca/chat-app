1eb584c658e8602fe761cc178c1caba944566c5b
Aleksandar Ristic <storiaca@gmail.com>
2019-10-10 13:39:44 +0200

    Create components Chat and Join

---
+++ client/src/components/Chat.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Chat = props => {
+  return (
+    <div>
+      <h1>Chat</h1>
+    </div>
+  );
+};
+
+Chat.propTypes = {};
+
+export default Chat;
---
+++ client/src/components/Join.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Join = props => {
+  return (
+    <div>
+      <h1>Join</h1>
+    </div>
+  );
+};
+
+Join.propTypes = {};
+
+export default Join;
