fe6c1cc2bc558d2bb71fef6f354e672678909acc
Aleksandar Ristic <storiaca@gmail.com>
2019-10-11 14:05:24 +0200

    Design Join component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 const Chat = props => {
   return (
@@ -8,7 +7,5 @@ const Chat = props => {
     </div>
   );
 };
-
-Chat.propTypes = {};
 
 export default Chat;
