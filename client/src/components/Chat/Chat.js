0fe4f8f4babe2249a020456a9d233a4696235558
Aleksandar Ristic <storiaca@gmail.com>
2019-10-11 14:34:14 +0200

    Configure Chat component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 
-const Chat = props => {
+let socket;
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const ENDPOINT = "localhost:5000";
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+    setName(name);
+    setRoom(room);
+
+    socket.emit("join", { name, room });
+  }, [ENDPOINT, location.search]);
+
   return (
     <div>
       <h1>Chat</h1>
