e356f14de6fd4d2ff813b680e58ffe486ea4c074
Aleksandar Ristic <storiaca@gmail.com>
2019-10-14 14:22:55 +0200

    Implement sending messages in Chat.js component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,11 +2,15 @@ import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
 
+import "./Chat.css";
+
 let socket;
 
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
   const ENDPOINT = "localhost:5000";
 
   useEffect(() => {
@@ -23,9 +27,32 @@ const Chat = ({ location }) => {
     };
   }, [ENDPOINT, location.search]);
 
+  useEffect(() => {
+    socket.on("message", message => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  const sendMessage = event => {
+    event.preventDefault();
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
+
+  console.log(message, messages);
+
   return (
-    <div>
-      <h1>Chat</h1>
+    <div className="outerContainer">
+      <div className="container">
+        <input
+          value={message}
+          onChange={event => setMessage(event.target.value)}
+          onKeyPress={event =>
+            event.key === "Enter" ? sendMessage(event) : null
+          }
+        />
+      </div>
     </div>
   );
 };
