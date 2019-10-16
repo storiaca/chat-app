980520aab582d18c55ffd49ee70893e7cd70c00e
Aleksandar Ristic <storiaca@gmail.com>
2019-10-16 10:49:18 +0200

    Add messages to disconnect and sendMessages

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -6,7 +6,7 @@ import "./Messages.css";
 
 const Messages = ({ messages, name }) => {
   return (
-    <ScrollToBottom>
+    <ScrollToBottom className="messages">
       {messages.map((message, i) => {
         return (
           <div key={i}>
