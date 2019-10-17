fe6992af170839b2e21ff7e718631a5303f56227
Aleksandar Ristic <storiaca@gmail.com>
2019-10-17 14:08:23 +0200

    change in .gitignore

---
+++ client/build/precache-manifest.c49a75226b54f4831025bfca076ab5a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7347a345778ebf7628f689c3804aa741",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6becb35595c276267f3f",
+    "url": "/static/css/main.f0f4ddc5.chunk.css"
+  },
+  {
+    "revision": "bca7ee9d72332bc01ba9",
+    "url": "/static/js/2.8aa95631.chunk.js"
+  },
+  {
+    "revision": "6becb35595c276267f3f",
+    "url": "/static/js/main.e36b1425.chunk.js"
+  },
+  {
+    "revision": "97b6691f1c08a5e5ad28",
+    "url": "/static/js/runtime-main.9a20ec70.js"
+  }
+]);
