679ee61ff79cfd421f774929c8de302b84cbba6f
Aleksandar Ristic <storiaca@gmail.com>
2019-10-17 13:17:20 +0200

    Create TextContainer component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -5,6 +5,7 @@ import io from "socket.io-client";
 import InfoBar from "../InfoBar/InfoBar";
 import Input from "../Input/Input";
 import Messages from "../Messages/Messages";
+import TextContainer from "../TextContainer/TextContainer";
 
 import "./Chat.css";
 
@@ -13,6 +14,7 @@ let socket;
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [users, setUsers] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
   const ENDPOINT = "localhost:5000";
@@ -35,6 +37,16 @@ const Chat = ({ location }) => {
     socket.on("message", message => {
       setMessages([...messages, message]);
     });
+
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+
+    return () => {
+      socket.emit("disconnect");
+
+      socket.off();
+    };
   }, [messages]);
 
   const sendMessage = event => {
@@ -57,6 +69,7 @@ const Chat = ({ location }) => {
           sendMessage={sendMessage}
         />
       </div>
+      <TextContainer users={users} />
     </div>
   );
 };
